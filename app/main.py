14f66a7d4ce03496ccf45009a407099543f87f51
Affan Mehmood <affanmehmood32@gmail.com>
2022-09-29 22:51:44 +0500

    bug test reverted fastapi 3

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import os
 import logging
 from threading import Thread
-
+from celery.result import AsyncResult
 from fastapi import FastAPI, BackgroundTasks
 
 from worker.celery_app import celery_app
+
 
 log = logging.getLogger(__name__)
 
@@ -14,14 +15,13 @@ app = FastAPI()
 def celery_on_message(body):
     log.warn(body)
 
+
 def background_on_message(task):
     log.warn(task.get(on_message=celery_on_message, propagate=False))
 
 
 @app.get("/{word}")
 async def root(word: str, background_task: BackgroundTasks):
-    task_name = None
-
     # set correct task name based on the way you run the example
     if not bool(os.getenv('DOCKER')):
         task_name = "app.worker.celery_worker.test_celery"
@@ -29,7 +29,16 @@ async def root(word: str, background_task: BackgroundTasks):
         task_name = "app.app.worker.celery_worker.test_celery"
 
     task = celery_app.send_task(task_name, args=[word])
-    print(task)
     background_task.add_task(background_on_message, task)
 
-    return {"message": "Word received"}
+    return {"task_id": task.task_id}
+
+
+@app.get("/get_progress/{task_id}")
+async def get_status(task_id: str):
+    res = AsyncResult(task_id)
+    print('res', res)
+    return {"progress": {
+        'state': res.state,
+        'meta': res.result
+    }}
--- app/worker/celery_worker.py
+++ app/worker/celery_worker.py
@@ -1,14 +1,15 @@
 from time import sleep
-
 from celery import current_task
-
 from .celery_app import celery_app
+from app.DALLE_DOCKER.infer import main
+import time
 
 
 @celery_app.task(acks_late=True)
 def test_celery(word: str) -> str:
-    for i in range(1, 50):
-        sleep(1)
-        current_task.update_state(state='PROGRESS',
-                                  meta={'process_percent': i*10})
-    return f"test task return {word}"
+    print('GEN Started')
+    current_task.update_state(state='GENERATING',
+                              meta={'Duration': 'unknown'})
+    main(word, outputs_dir='/app/dalle_tmp/')
+
+    return f"Generation completed {word} {current_task.task_id}"
