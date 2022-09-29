3a1b8fab35e867b42db860b3c04e7220398b370a
Affan Mehmood <affanmehmood32@gmail.com>
2022-09-29 23:28:27 +0500

    bug test reverted fastapi fixed

--- app/worker/celery_worker.py
+++ app/worker/celery_worker.py
@@ -17,6 +17,5 @@ def test_celery(word: str) -> str:
     print('GEN Started')
     current_task.update_state(state='GENERATING',
                               meta={'Duration': 'Dalle Running'})
-    main(word, outputs_dir='/app/dalle_tmp/', task_id=current_task.task_id)
-
+    main(word, outputs_dir='/app/dalle_tmp/', task_id=current_task)
     return f"Generation completed {word}"
